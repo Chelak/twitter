75bff387cb2a59f7f9241092fa6b62599cce829a
Chelak Serghey <celaserg@gmail.com>
2015-08-13 16:18:32 +0300

    Home controller and home page

--- src/main/java/com/app/mytw/controller/HomePageController.java
+++ src/main/java/com/app/mytw/controller/HomePageController.java
@@ -1,10 +1,21 @@
 package com.app.mytw.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by user on 8/13/2015.
  */
 
+@Controller
+@RequestMapping({"/", "home"})
 public class HomePageController
 {
 
+        @RequestMapping(method = RequestMethod.GET)
+        public String getHomepage()
+        {
+            return "index";
+        }
 }
---
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+
+</head>
+<body>
+<h1>
+    Homepage
+</h1>
+
+</body>
+</html>
